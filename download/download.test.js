54be053f2dee263d0a54dff55afc0ddfc047a2fc
Brian Duffey <brianduffey@Brians-MacBook-Pro.local>
2018-08-22 13:49:42 -0500

    Fixed download test

--- download/download.test.js
+++ download/download.test.js
@@ -2,7 +2,7 @@ const { getNowInSeconds }= require('./download');
 
 test('Get date in seconds works as expected', () => {
 	const dt = new Date(2018, 7, 22);
-	const expected = Math.floor(dt.getTime() / 1000);
+	const expected = 1534914000;
 
 	expect(getNowInSeconds(dt)).toBe(expected);
 });
