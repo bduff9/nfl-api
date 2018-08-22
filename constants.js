e3d7762508b01ed93b070f0539497e5571bcec07
Brian Duffey <brianduffey@Brians-MacBook-Pro.local>
2018-08-22 13:23:11 -0500

    Added central constants file

---
+++ constants.js
@@ -0,0 +1,14 @@
+const DB_FILE = '../db.json';
+
+const BETWEEN_GAMES_MIN = 60;
+
+const HIGH_SCORE = 64;
+
+const LOW_SCORE = 0;
+
+module.exports = {
+	BETWEEN_GAMES_MIN,
+	DB_FILE,
+	HIGH_SCORE,
+	LOW_SCORE,
+};
